3fabfaba836d7807b21e2ccf4773cedfd3867c02
Marcos Kichel <marcos.kichel@gmail.com>
2026-04-04 04:19:36 -0300

    chore: use neverthrow by default

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import baseConfig from './packages/eslint-config/base.mjs';
 import nodeConfig from './packages/eslint-config/node.mjs';
+import neverthrowConfig from './packages/eslint-config/neverthrow.mjs';
 
 export default [
   { ignores: ['**/dist/**', '**/node_modules/**', '**/vitest.config.ts', '**/eslint.config.mjs'] },
   ...baseConfig,
   ...nodeConfig,
+  ...neverthrowConfig,
 ];
