15635ce9ea5bd7c2c3fb17e9976e546f14a9beda
Marcos Kichel <marcos.kichel@gmail.com>
2026-04-05 00:04:58 -0300

    chore: remove @neurokit/engram package
    Package functionality is covered by @neurokit/ltm and @neurokit/stm.

--- packages/engram/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import baseConfig from '@neurokit/eslint-config/base';
-import nodeConfig from '@neurokit/eslint-config/node';
-
-export default [
-  { ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs', 'vitest.config.ts'] },
-  ...baseConfig,
-  ...nodeConfig,
-];
--- packages/engram/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { EngramEngine } from './engram-engine.js';
-export type { EngramOptions, EngramRecord } from './engram-engine.js';
-
-import { EngramEngine, type EngramOptions } from './engram-engine.js';
-
-export function createEngramEngine(options: EngramOptions = {}): EngramEngine {
-  return new EngramEngine(options);
-}
